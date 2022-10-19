2038d3da393182dee4a97200ddeb0adc03eaa588
Will Vining <awvining91@gmail.com>
2022-10-19 16:09:32 -0700

    updated note functions

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -11,8 +11,8 @@ const {
 //to create or delete a note
 //The Delete operator https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/delete
 const {
-    noteCreateNewNote,
-    noteDeleteNote
+    addReminder,
+    eraseReminder
 } = require('../../lib/noteFunctions');
 
 //save note history to json db 
@@ -28,12 +28,12 @@ router.post('/notes', (req, res) => {
     //req.body holds params that are sent from the client as part of a POST request
     //https://www.google.com/search?q=req.body.id&rlz=1C5CHFA_enUS956US956&sxsrf=AOaemvKGY312QoUwkH4PqOMtWmVDSP7SNA%3A1631569829576&ei=pcc_YYy3IonA0PEPkJyr0AI&oq=req.body.id&gs_lcp=Cgdnd3Mtd2l6EAMyBQgAEIAEMgUIABCABDIECAAQHjIGCAAQBRAeMgYIABAFEB4yBAgAEB46BwgAEEcQsANKBAhBGABQ-5IIWPuSCGDpoAhoAXACeACAAXmIAXmSAQMwLjGYAQCgAQKgAQHIAQjAAQE&sclient=gws-wiz&ved=0ahUKEwjMnfig9_zyAhUJIDQIHRDOCioQ4dUDCA4&uact=5
     req.body.id = notes.length.toString();
-    let note = noteCreateNewNote(req.body, notes);
+    let note = addReminder(req.body, notes);
     res.json(note);
 })
 
 router.delete('/notes/:id', (req, res) => {
-    noteDeleteNote(notes, req.params.id);
+    eraseReminder(notes, req.params.id);
     res.json(notes);
  })
 
